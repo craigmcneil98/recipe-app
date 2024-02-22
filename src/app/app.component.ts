b3b3fd6661712cd77ee6d299fe35e50d9e6fb5a6
Craigmcneil98 <Cmcnei301@caledonian.ac.uk>
2024-02-22 20:40:58 +0000

    Changes to the App component files to accommodate the Navbar and Footer components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  template: `
+            <app-navbar />
+            <router-outlet />
+            <app-footer />
+            `,
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'recipe-app';
 }
